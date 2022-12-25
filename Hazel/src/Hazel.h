637d3cd9c269ed5d3416f3f8049c1b6029486d0b
ringo199 <notbadapple@gmail.com>
2022-12-25 11:43:00 +0800

    pre learn opengl

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,6 +2,14 @@
 
 // For use by Hazel applications
 #include "Hazel/Application.h"
+#include "Hazel/Layer.h"
+#include "Hazel/Log.h"
+
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
+
+#include "Hazel/ImGui/ImGuiLayer.h"
 
 // ---Entry Point---------------------
 #include "Hazel/EntryPoint.h"
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,46 @@
 #include <Hazel.h>
 
+#include "imgui/imgui.h"
+
+class ExampleLayer : public Hazel::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
+			HZ_INFO("Tab key is pressed!(poll)");
+	}
+
+	virtual void OnImGuiRender() override
+	{
+		//ImGui::Begin("Test");
+		//ImGui::Text("Hello World");
+		//ImGui::End();
+	}
+
+	void OnEvent(Hazel::Event& event) override
+	{
+		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		{
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			if (e.GetKeyCode() == HZ_KEY_TAB)
+				HZ_TRACE("Tab key is pressed!(event)");
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+		}
+	}
+};
+
 class Sandbox : public Hazel::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox()
